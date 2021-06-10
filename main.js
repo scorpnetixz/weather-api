bbbf6620e20274ff0e29e142c35fdbde0df112d7
scorpnetixz <scorpnetixz@gmail.com>
2021-06-10 06:49:25 +0300

    forth commit

--- main.js
+++ main.js
@@ -3,37 +3,41 @@ document.querySelector("#form").addEventListener("submit", loadWeather);
 let unit;
 let radio = document.querySelectorAll("input[type=radio]");
 
-for(let i = 0; i < radio.length; i++){
-    radio[i].addEventListener("change", function(){
+for (let i = 0; i < radio.length; i++) {
+    radio[i].addEventListener("change", function () {
         unit = this.value;
-    })
-};
+    });
+}
 
-function loadWeather(e){
+function loadWeather(e) {
     e.preventDefault();
-    let getWeather = async ()=> {
+    let getWeather = async () => {
         let city = document.querySelector("#city").value;
         let h2 = document.querySelector(".header h2");
         h2.innerHTML = city.charAt(0).toUpperCase() + city.slice(1);
         console.log(unit);
         let newUnit;
-        if(unit === "C"){
+        if (unit === "C") {
             newUnit = "metric";
-        }else if(unit === "F"){
+        } else if (unit === "F") {
             newUnit = "imperial";
         }
         let key = "6501b9a4381a4e7eb6901f6addc12612";
-        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${newUnit}&appid=${key}`);
-        if(response.status !== 200){
+        let response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${newUnit}&appid=${key}`
+        );
+        if (response.status !== 200) {
             throw new Error("couldn't fetch weather");
-        };
+        }
         let data = response.json();
         return data;
     };
-    getWeather().then(data => {
-        document.querySelector(".column-1 h1").innerHTML = data.main.temp + `<sup>o</sup><span>${unit}</span>`;
-        document.querySelector(".column-2 .feels").innerHTML = data.main.feels_like + `<sup>o</sup><span>${unit}</span>`;
-        document.querySelector(".column-2 .wind").innerHTML = data.wind.speed + "%";
-
-    })
+    getWeather().then((data) => {
+        document.querySelector(".column-1 h1").innerHTML =
+            data.main.temp + `<sup>o</sup><span>${unit}</span>`;
+        document.querySelector(".column-2 .feels").innerHTML =
+            data.main.feels_like + `<sup>o</sup><span>${unit}</span>`;
+        document.querySelector(".column-2 .wind").innerHTML =
+            data.wind.speed + "%";
+    });
 }
